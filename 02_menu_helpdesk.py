b6e1fc6fdcc640a434f5eadaf9c27f071183bb64
Osvin <osvingamez94@gmail.com>
2026-08-13 21:17:38 -0600

    feat: menu creado

---
+++ 02_menu_helpdesk.py
@@ -0,0 +1,11 @@
+def pedir_opcion():
+    print("\n" + "="*30)
+    print("      MENÚ DE HELPDESK")
+    print("="*30)
+    print("1. Registrar ticket")
+    print("2. Listar tickets")
+    print("3. Buscar por solicitante")
+    print("4. Mostrar resumen por prioridad")
+    print("5. Salir")
+    return input("Seleccione una opción (1-5): ")
+
